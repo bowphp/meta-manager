fe3a8b0b4c5cb3258431f61dc8499727ff4ec536
Franck DAKIA <dakiafranck@gmail.com>
2023-03-15 10:51:36 +0000

    Init Bow web app meta manager

---
+++ src/MetaManagerConfiguration.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Bow\MetaManager;
+
+use Bow\View\View;
+use Tintin\Tintin;
+use Bow\Configuration\Configuration;
+use Bow\Configuration\Loader as Config;
+
+class MetaManagerConfiguration extends Configuration
+{
+    /**
+     * @inheritDoc
+     */
+    public function create(Config $config): void
+    {
+        $meta = (array) $config['meta'];
+
+        $meta = array_merge(
+            require __DIR__ . '/config/meta.php',
+            $meta
+        );
+
+        $config['meta'] = $meta;
+
+        /**@var Tintin */
+        $template = View::getInstance()->getTemplate();
+
+        $template->directive('meta', function(array $attribues) use ($tempate) {
+            $data = $attribues[0] ?? [];
+            return $tempate->renderString(file_get_contents(__DIR__.'/views/meta.tintin.php'), $data);
+        });
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function run(): void
+    {
+        //
+    }
+}
