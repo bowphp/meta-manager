c1477e2c0cedf9904124826e2f269d7b04e34657
Franck DAKIA <dakiafranck@gmail.com>
2023-04-22 17:17:27 +0000

    Fixes the directive compilation

--- tests/MetaManagerTest.php
+++ tests/MetaManagerTest.php
@@ -3,17 +3,21 @@
 namespace Test;
 
 use Bow\View\View;
+use Spatie\Snapshots\MatchesSnapshots;
 use Tintin\Compiler;
 use Tintin\Tintin;
 
 class MetaManagerTest extends \PHPUnit\Framework\TestCase
 {
+    use MatchesSnapshots;
+
     public function testCompileTheMetaView()
     {
         $compiler = new Compiler();
         $output = $compiler->compile(file_get_contents(__DIR__.'/../src/views/meta.tintin.php'));
 
         $this->assertStringContainsString('<meta property="fb:app_id" content="<?php echo htmlspecialchars(config(\'meta.fb_app_id\'), ENT_QUOTES); ?>"/>', $output);
+        $this->assertMatchesTextSnapshot($output);
     }
 
     public function testCompileTheSpecialDirectiveStructure()
@@ -25,7 +29,8 @@ class MetaManagerTest extends \PHPUnit\Framework\TestCase
         $tintin = View::getInstance()->getEngine();
         $output = $tintin->getCompiler()->compile('%meta');
 
-        $this->assertEquals(trim($output), '<?php echo $__tintin->getCompiler()->_____executeCustomDirectory("meta");');
+        $this->assertEquals(trim($output), '<?php echo $__tintin->getCompiler()->_____executeCustomDirectory("meta"); ?>');
+        $this->assertMatchesTextSnapshot($output);
     }
 
     public function testCompileTheSpecialDirectiveStructureWithParam()
@@ -37,7 +42,8 @@ class MetaManagerTest extends \PHPUnit\Framework\TestCase
         $tintin = View::getInstance()->getEngine();
         $output = $tintin->getCompiler()->compile('%meta(["title" => "Yoo"])');
 
-        $this->assertEquals(trim($output), '<?php echo $__tintin->getCompiler()->_____executeCustomDirectory("meta", ["title" => "Yoo"]);');
+        $this->assertEquals(trim($output), '<?php echo $__tintin->getCompiler()->_____executeCustomDirectory("meta", ["title" => "Yoo"]); ?>');
+        $this->assertMatchesTextSnapshot($output);
     }
 
     public function testCompileTheSpecialDirectiveStructureWithParamAndLinebracken()
@@ -51,6 +57,9 @@ class MetaManagerTest extends \PHPUnit\Framework\TestCase
             "title" => "Yoo"
         ])');
 
-        $this->assertEquals(trim($output), '<?php echo $__tintin->getCompiler()->_____executeCustomDirectory("meta", ["title" => "Yoo"]);');
+        $this->assertEquals(trim($output), '<?php echo $__tintin->getCompiler()->_____executeCustomDirectory("meta", [
+            "title" => "Yoo"
+        ]); ?>');
+        $this->assertMatchesTextSnapshot($output);
     }
 }
